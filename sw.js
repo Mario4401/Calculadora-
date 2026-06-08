bf156fe1456de86fe0f0e7b540df7cde01742fb9
Mario4401 <mariojimenezgallego@gmail.com>
2026-06-08 22:59:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-const CACHE_NAME = 'trading-risk-v1';
-const FILES = ['./', './index.html', './manifest.json', './sw.js', './icon.svg'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
+const CACHE_NAME='trading-risk-v2';const FILES=['./','./index.html','./manifest.json','./sw.js','./icon.svg'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))));self.clients.claim()});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
